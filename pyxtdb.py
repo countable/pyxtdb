34a20100ca316c5f1adbc582c1d40ea9a5de4506
Clark Van Oyen <clark@countable.ca>
2022-03-07 23:03:01 -0800

    syntax sugar

--- pyxtdb.py
+++ pyxtdb.py
@@ -1,9 +1,68 @@
 import requests
+import json
+
+class AlreadySent(Exception):
+    pass
+
+class Query:
+    def __init__(self, uri="http://localhost:3000", node=None):
+        if node:
+            self.node = node
+        else:
+            self.node = Node(uri)
+        self._find_clause = None
+        self._where_clauses = []
+        self._values = None
+        self.error = None
+    
+    def find(self, clause):
+        if self._values:
+            raise AlreadySent()
+        self._find_clause = clause
+        return self
+
+    def where(self, clause):
+        if self._values:
+            raise AlreadySent()
+        self._where_clauses.append(clause)
+        return self
+
+    def values(self):
+        if not self._where_clauses:
+            raise Exception("No Where Clause")
+        _query = """
+        {
+            :find [%s]
+            :where [
+                [%s]
+            ]
+        }
+        """ % (self._find_clause, ']\n['.join(self._where_clauses))
+       
+        result = self.node.query(_query)
+        if type(result) is dict:
+            self.error = json.dumps(result, indent=4, sort_keys=True)
+            return []
+        return result
+
+    def __iter__(self):
+        self._values = self.values()
+        return self
+    
+    def __next__(self):
+        if len(self._values):
+            return self._values.pop()
+        else:
+            raise StopIteration
 
 
 class Node:
     def __init__(self, uri="http://localhost:3000"):
         self.uri = uri
+
+    def find(self, find_clause):
+
+        return Query(node=self).find(find_clause)
 
     def call_rest_api(self, action, params):
 
@@ -40,6 +99,7 @@ class Node:
         return self.submitTx(transaction)
 
     def query(self, query):
+        query=query.strip()
         # basic syntax check
         assert query.startswith("{") and query.endswith("}")
 
--- tests.py
+++ tests.py
@@ -1,31 +1,59 @@
 import pytest
-
+import os
 import pyxtdb
+
+XTDB_URL = os.environ.get('XTDB_URL', 'http://localhost:3000')
 
 @pytest.fixture
 def node():
-    return pyxtdb.Node()
+    return pyxtdb.Node(XTDB_URL)
+
+def billies(result):
+    return len([rec for rec in result if rec[0]=='billy'])
 
 def test_put_and_query(node):
-
     # Remove record with xt/id "billy", check it's gone.
     node.evict("billy")
     # Fetch ALL records
     result = node.query(r"{:find [?e], :where [[?e :xt/id]]}")
-    # One should have xt/id "billy"
-    assert len([rec for rec in result if rec[0]=='billy']) == 0
+    # None should have xt/id "billy"
+    assert billies(result) == 0
 
     # Create a record and ensure it's found.
     node.put({"xt/id": "billy", "name": "Billy", "last-name": "Idol"})
     # Fetch ALL records
     result = node.query(r"{:find [?e], :where [[?e :xt/id]]}")
     # One should have xt/id "billy"
-    assert len([rec for rec in result if rec[0]=='billy']) == 1
+    assert billies(result) == 1
 
     # Ensure it's gone again after deletion.
     node.evict("billy")
     # Fetch ALL records
     result = node.query(r"{:find [?e], :where [[?e :xt/id]]}")
-    # One should have xt/id "billy"
-    assert len([rec for rec in result if rec[0]=='billy']) == 0
+    # None should have xt/id "billy"
+    assert billies(result) == 0
 
+def test_query_model(node):
+
+    # Remove record with xt/id "billy", check it's gone.
+    node.evict(1)
+    node.evict(2)
+    node.evict(3)
+    # Fetch ALL records
+    result = node.find("?e").where("?e :xt/id")
+    # None should have xt/id "billy"
+    assert len(list(result)) == 0
+
+    # Create a record and ensure it's found.
+    node.put({"xt/id": 1, "name": "Billy", "last-name": "Idol"})
+    node.put({"xt/id": 2, "name": "Billy", "last-name": "Joel"})
+    node.put({"xt/id": 3, "name": "Billy", "last-name": "Bob"})
+    
+    # Fetch ALL records
+    result = node.find("?e").where("?e :xt/id")
+    assert len(list(result)) == 3
+
+    result = node.find("?e").where('?e :last-name "Joel"')
+    assert len(list(result)) == 1
+    assert result.error == None
+    print(result)
